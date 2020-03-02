c5d4d5255132ca57ff38992555d45c3b49d7be5f
Mark Capodagli <mcapodagli@amuniversal.com>
2020-03-02 17:32:43 -0600

    typo

--- clock.rb
+++ clock.rb
@@ -1,6 +1,6 @@
 require 'clockwork'
 include Clockwork
 
-every(5.minutes, 'Send Messages', , :at => ['**:00', '**:05', '**:10', '**:15', '**:20', '**:25', '**:30', '**:35', '**:40', '**:45', '**:50', '**:55']) {
+every(5.minutes, 'Send Messages', :at => ['**:00', '**:05', '**:10', '**:15', '**:20', '**:25', '**:30', '**:35', '**:40', '**:45', '**:50', '**:55']) {
   `rake reservations:process`
 }
